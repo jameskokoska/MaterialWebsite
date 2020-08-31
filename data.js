01223514fd77a8bdeaf93630e470860bf8f95988
jameskokoska <jameskokoska@users.noreply.github.com>
2020-08-31 02:17:10 -0400

    changed to read from data set with js

---
+++ data.js
@@ -0,0 +1,25 @@
+var welcomeMessage = "Welcome";
+var aboutmeTitle = "Hello I'm Person!";
+var aboutmeContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec elit a arcu imperdiet euismod ac ac nisi. Praesent accumsan ac massa eu ultrices. Aliquam sodales accumsan lorem, id mollis nulla auctor sed. Mauris ac aliquam quam. Curabitur metus justo, congue blandit ipsum in, iaculis auctor dolor. Nam sed ullamcorper metus. In porttitor sem eget neque malesuada volutpat. Vivamus ultricies tellus vitae venenatis eleifend. Sed porta tincidunt congue. Suspendisse enim ex, placerat ac ultricies in, aliquet ac nisl. Nunc vulputate pretium augue at fermentum. Curabitur sollicitudin nunc a ante vulputate, id varius metus blandit.";
+var experiencePoints = [
+    "Vivamus vel ultrices turpis, at consectetur odio.",
+    "In efficitur hendrerit lorem non auctor.",
+    "Vivamus ac lorem vehicula, feugiat orci et, convallis libero.",
+    "Morbi eget leo vitae lorem fermentum convallis a non turpis.",
+    "Aenean dictum ipsum eget dignissim dictum.",
+];
+var contactContent = "Pellentesque eu tellus elementum, mollis nunc eget, sollicitudin sapien. Maecenas accumsan finibus ipsum euismod ornare. Curabitur elit diam, congue non blandit sed, accumsan sed tortor. Vestibulum vitae turpis ultrices, elementum mi in, tristique elit. Nullam ornare eros sed libero gravida volutpat. Proin at vehicula eros. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.";
+var contact = [
+    {
+        "fontawesomeCode":"fab fa-linkedin-in",
+        "link":"https://ca.linkedin.com/",
+    },
+    {
+        "fontawesomeCode":"fas fa-envelope",
+        "link":"https://www.google.com/",
+    },
+    {
+        "fontawesomeCode":"fab fa-facebook-f",
+        "link":"https://www.facebook.com/",
+    },
+]
